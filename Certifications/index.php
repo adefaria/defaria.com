c0f986cdaa42f59e8bb548a9e5ad9b5991763aca
Andrew DeFaria <Andrew@DeFaria.com>
2021-02-15 13:54:29 -0800

    Adding certs

---
+++ Certifications/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+  <meta name="verify-v1" content="4HQ9/7f2knFu9nL7tb3s4TPbozxnE2H7LPRj9txmtbk=" />
+  <title>Andrew DeFaria</title>
+  <link rel="stylesheet" type="text/css" media="screen" href="/css/Default.css">
+  <link rel="stylesheet" type="text/css" media="print" href="/css/Print.css">
+  <link rel="SHORTCUT ICON" href="https://defaria.com/favicon.ico" type="image/png">
+
+  <!-- Google Analytics -->
+  <script src="https://www.google-analytics.com/urchin.js" type="text/javascript">
+  </script>
+  <script type="text/javascript">
+    _uacct = "UA-89317-1";
+    urchinTracker ();
+  </script>
+  <!-- Google Analytics -->
+
+  <?php include "site-functions.php"?>
+  <?php include "oneliner.php"?>
+</head>
+
+<body>
+
+<div class="heading">
+  <h1 class="centered">Andrew DeFaria's Certifications</h1>
+</div>
+
+<div id="content">
+  <?php navigation_bar ()?>
+
+  <h2>Certifications and Classes Attended</h2>
+
+  <ul>
+    <li>AWS Solutions Architect Certificate</li>
+    
+    <li><a href="https://verify.acloud.guru/460BD1352CF9">Jenkins Quick Start Class</a> - Classroom</li>
+    
+    <li><a href="https://verify.acloud.guru/3D5DC32F0FE2">Jekins Fundamentals</a> - Classroom</li>
+  
+    <li><a href="https://verify.acloud.guru/BAA05376C3FA">Docker Certified Associate (DCA)</a> - Classroom</li>
+  </ul>
+  
+  <?php copyright ("1960");?>
+</div>
+
+</body>
+</html>
