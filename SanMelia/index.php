fb831c21ad11736419d9eb732783bc40b49aedb7
Andrew DeFaria <Andrew@DeFaria.com>
2021-11-02 09:06:13 -0700

    Added SanMelia and phoneinfoga

---
+++ SanMelia/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+  <meta name="verify-v1" content="4HQ9/7f2knFu9nL7tb3s4TPbozxnE2H7LPRj9txmtbk=" />
+  <title>San Melia</title>
+  <link rel="SHORTCUT ICON" href="http://defaria.com/favicon.ico" type="image/png">
+
+  <!-- Google Analytics -->
+  <script src="https://www.google-analytics.com/urchin.js" type="text/javascript">
+  </script>
+  <script type="text/javascript">
+    _uacct = "UA-89317-1";
+    urchinTracker ();
+  </script>
+  <!-- Google Analytics -->
+
+  <?php include "site-functions.php"?>
+  <?php include "oneliner.php"?>
+</head>
+
+<body>
+
+  <h1 class="centered">An Example of Bad Management</h1>
+</div>
+
+<div id="content">
+  <h2>My Review of San Melia</h2>
+  <p>Man this place has really gone downhill, especially since they were bought out by Avenue5. Service sucks, you have to wait days for them to fix the simplest thing. They advertise WiFi at the pool and have 4 access points. None of them work! For the 3 years I was here I was never able to get a connection. I asked others at the pool whether or not they were using the WiFi. None were. They were either using their own WiFi hot spots or their cellular data. My phone has terrible coverage in this area so no WiFi for me.
+
+I saw a Cox van in the area and asked him about why these WiFi access points were not working. The WiFis listed but when you connected to them my phone said: "Connected with No Internet". The Cox guy said he'd ask around. I later emailed because the WiFi got no better. His response was to talk to the office. The office is clueless - FOR 3 YEARS I battled this and got nowhere. I sent them multiple emails about this but they just ignored me. When you advertise a service but fail to provide it for years even after being asked to fix it but all you do is ignore it, you're a fucking asshole!</p>
+
+<p>I made automated payments through their system but when Avenue5 bought out the previous owners they didn't support this. The website was dead for months. Avenue5 had no plans to transition this stuff. The office said they put notices on all the apartment doors saying that autopay was not working and to play manually but I never got one. So my payment was not made and on the 4th of the month, they tacked a 30 day notice on my door. They piled on about $400 of additional late charges. I had to bitch, moan and argue my ass off to get them to refund those additional charges because it was not my fault that the payment wasn't made. And they made me pay via money order!!! WTF?!? It was you who canceled the auto payment system without notifying me and now you don't trust my check and require a money order? Fuck you!</p>
+
+<p>They send you tons of spam emails about stupid shit from the same email address that they send business-related emails. I'm not here to receive your fucking spam. I told them to simply make another email address and send the business email from one and the spam from another. In 3 damn years, they were unable to do that so I blocked them from emailing me telling them I didn't want their fucking spam. They had their lawyer send me a cease and desist letter saying I was sending "threatening" emails. My emails were not threatening them with anything other than disallowing them from sending me any more emails.</p>
+
+<p>At the start of Covid they closed the pool for months. While they are not responsible for Covid, part of my rent goes to the common areas and to use such facilities as the pool and the WiFi I was not able to use the pool for about 3 or 4 months and the WiFi for 3 years. No reimbursement for me or anybody else!</p>
+
+<p>For about a year I had no communications with them except one in which I inquired about when they were going to reopen the pool. That's all.</p>
+
+<p>Then the other day I was awoken by "the leaf blowers". You know them. You hear them all the time. But this was at 6:30 Am and it was still dark. So I wrote them an angry email saying:</p>
+
+<blockquote>
+Look I have enough problems trying to get some decent sleep. Could you at least keep the leaf blowers at bay until the FUCKING SUN COMES UP!?!<br>
+It's 6:29 am and the asshole's right outside my bedroom door blowing away and sunrise today is scheduled for 7:09 am.<br>
+
+Geeze fucking christ!<br>
+</blockquote>
+
+<p>Then, 4 days later I find a notice on my door saying I have 30 days to leave!</p>
+
+<p>Ask yourself this, "Will I be the next person treated badly by the office and asked to leave?".</p>
+
+</div>
+
+</body>
+</html>
