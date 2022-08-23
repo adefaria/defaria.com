e421d7958e9add0fca345cdcfbeb60692e2060a6
Andrew DeFaria <A.DeFaria@cpanel.net>
2022-08-23 13:15:39 -0700

    Added sunline review

---
+++ sunline/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+  <meta name="verify-v1" content="4HQ9/7f2knFu9nL7tb3s4TPbozxnE2H7LPRj9txmtbk=" />
+  <title>Andrew DeFaria</title>
+  <link rel="stylesheet" type="text/css" media="screen" href="/css/Default.css">
+  <link rel="stylesheet" type="text/css" media="print" href="/css/Print.css">
+  <link rel="SHORTCUT ICON" href="https://defaria.com/favicon.ico" type="image/png">
+
+  <!-- Google Analytics -->
+  <script src="https://www.google-analytics.com/urchin.js" type="text/javascript">
+  </script>
+  <script type="text/javascript">
+    _uacct = "UA-89317-1";
+    urchinTracker ();
+  </script>
+  <!-- Google Analytics -->
+
+  <?php include "site-functions.php"?>
+  <?php include "oneliner.php"?>
+</head>
+
+<body>
+
+<div class="heading">
+  <h1 class="centered">A run in with Sunline Energy</h1>
+</div>
+
+<div id="content">
+  <?php navigation_bar ()?>
+
+  <h2>A run in with Sunline Energy</h2>
+
+  <p>So I'm looking at buying a house and I'm concerned with its roof. It seems customary
+  to get 3 roof inspections and I decided to use Sunline Engery for one of them. They
+  were highly rated in yelp - 348 reviews and an average of 5 stars. But as we can see below
+  that may be because Mark scares off anybody who gives a negative review.</p>
+
+  <p>So I had Sunlight Solar come out to give me an estimate of a roof on a house that
+  I'm in the process of buying. The guy, Jacob Smith, came out and we spoke for a while.
+  I mentioned that I didn't think the seller would replace the roof but rather just offer
+  a credit. This seemed to irk Jacob because he didn't think this would turn out to be a
+  job for his company, that it would just be the estimate. Sorry but for all I know the
+  sellers may take that estimate and hire your company. Or I may hire your company down
+  the road too. The point is you should provide the estimate, not a text message.</p>
+
+  <p>He said that they take the satellite images to do the inspection so they don't risk
+  breaking any of the concrete tiles. I didn't know satellite images were that detailed.
+  Jacob said he would email me with the results.</p>
+
+  <p>Instead, he simply texts me a number. Well, this was not what I was expecting and I had
+  to call/text him and insist that I get a proper report.</p>
+
+  <p>I had also called the main number of Sunlight Solar and asked if this was how they
+  regularly do this. The guy didn't seem to know the SOP of his own company.</p>
+
+  <p>Eventually, I got the report in the report in my email but I don't like having to pull
+  teeth and play the part of the angry customer when this all should have been simply
+  handled professionally  in the first place!</p>
+
+  <p>So the owner, Mark, 858-977-2408, calls me up and argues with me for 10 minutes and
+  threatens to call the listing agent supposedly to say "Ah the roof is fine" unless I
+  remove this review. He must be trying to sabotage my negotiations as I can see no other
+  reason for the owner of Sunline to call the agent. Surely he's not calling them to say
+  that the roof needs replacing. Feel free to call him up and ask why he's doing this.</p>
+
+  <p>What a slimeball! Let's see what other rating sites I can spread this message to...</p>
+
+  <p>Yelp may decide to have this review taken down however it will live forever on my
+  server at https://defaria.com/sunline.</p>
+  <?php copyright ("1960");?>
+</div>
+
+</body>
+</html>
