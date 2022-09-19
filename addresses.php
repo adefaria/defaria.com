fd40c3d9a78823887230740d28a2688ed9bfe522
Andrew DeFaria <A.DeFaria@cpanel.net>
2022-09-19 13:56:02 -0700

    Updated addresses

--- addresses.php
+++ addresses.php
@@ -32,7 +32,15 @@
   <ol>
     <li>
       <address>
-        <a href="https://goo.gl/maps/6f1hokvFMTUa2fq8A">
+        <a href="https://www.google.com/maps/place/2010+W+San+Marcos+Blvd,+San+Marcos,+CA+92078/@33.1349147,-117.2193893,1205m/data=!3m1!1e3!4m13!1m7!3m6!1s0x80dc75212f3c9fff:0x103842edc2818238!2s2010+W+San+Marcos+Blvd+UNIT+33,+San+Marcos,+CA+92078!3b1!8m2!3d33.1349147!4d-117.2172006!3m4!1s0x80dc752128c85183:0x191a5e07f6d59b1a!8m2!3d33.1349083!4d-117.2172925">
+	2010 West San Marcos, Unit 33<br>
+	San Marcos, California 92078<br></a>
+	September 2022 - Eternity!
+      </address>
+    </li>
+    <li>
+      <address>
+        <a href="https://www.google.com/maps/place/33%C2%B011'29.8%22N+117%C2%B015'38.6%22W/@33.1916195,-117.2606248,601m/data=!3m1!1e3!4m5!3m4!1s0x0:0xcda2779b56be1f56!8m2!3d33.1916215!4d-117.2606765?hl=en">
 	870 Soft Wind Road #4<br>
 	Vista, California 92081<br></a>
 	January 2021 - Present
@@ -40,7 +48,7 @@
     </li>
     <li>
       <address>
-        <a href="https://goo.gl/Jp596u">
+        <a href="https://www.google.com/maps/place/33%C2%B018'54.6%22N+111%C2%B058'48.3%22W/@33.3150231,-111.9810754,393m/data=!3m1!1e3!4m14!1m7!3m6!1s0x872b04410e36d965:0xe43cdef39821efa!2s14435+S+48th+St+APT+2083,+Phoenix,+AZ+85044!3b1!8m2!3d33.3145128!4d-111.9785829!3m5!1s0x0:0x0!7e2!8m2!3d33.3151628!4d-111.9800831?hl=en">
 	14435 S. 48th Street #2083<br>
 	Phoenix, Arizona 85044-6448<br></a>
 	March 2018 - January 2021
@@ -48,7 +56,7 @@
     </li>
     <li>
       <address>
-        <a href="https://goo.gl/maps/xeiEF3hCQ6F2">
+        <a href="https://www.google.com/maps/place/11727+Stoney+Peak+Dr,+San+Diego,+CA+92128/@32.9790764,-117.0795625,363m/data=!3m2!1e3!4b1!4m13!1m7!3m6!1s0x80dbfa0203a4507f:0xd03dcba919c96fd2!2s11727+Stoney+Peak+Dr,+San+Diego,+CA+92128!3b1!8m2!3d32.9790683!4d-117.0789191!3m4!1s0x80dbfa0203a4507f:0xd03dcba919c96fd2!8m2!3d32.9790683!4d-117.0789191?hl=en">
         11727 Stoney Peak Drive #65<br>
         San Diego, California 92128-6440<br></a>
         August 2016 - March 2018
@@ -56,7 +64,7 @@
     </li>
     <li>
       <address>
-        <a href="https://goo.gl/uSZizy">
+        <a href="https://www.google.com/maps/place/33%C2%B058'46.9%22N+118%C2%B027'40.7%22W/@33.9796109,-118.4646305,1548m/data=!3m1!1e3!4m5!3m4!1s0x0:0xc75e1c3d494932cf!8m2!3d33.9796944!4d-118.4613056?hl=en">
         4139 Via Marina #709<br>
         Marina Del Rey, California 90292-5385<br></a>
         May 2016 - August 2016
@@ -64,7 +72,7 @@
     </li>
     <li>
       <address>
-        <a href="https://goo.gl/NL29o">
+        <a href="https://www.google.com/maps/place/1676+Hope+Dr+%231915,+Santa+Clara,+CA+95054/@37.399244,-121.957555,1143m/data=!3m1!1e3!4m5!3m4!1s0x808fc9a33d5aedaf:0x660a60b1bdd7a538!8m2!3d37.3989743!4d-121.9573017?hl=en">
 	1676 Hope Drive #1915<br>
 	Santa Clara, California 95054-1721<br></a>
 	July 2013 - May 2016
@@ -72,7 +80,7 @@
     </li>
     <li>
       <address>
-        <a href="https://g.co/maps/4xg7a">
+        <a href="https://www.google.com/maps/place/11064+W+Ocean+Air+Dr+%23230,+San+Diego,+CA+92130/@32.916809,-117.2297363,1207m/data=!3m1!1e3!4m5!3m4!1s0x80dc06518bad1a2f:0xca858b3eba35ed16!8m2!3d32.916809!4d-117.2275476">
         11064 West Ocean Air Drive #230<br>
         San Diego, California 92130-4605<br></a>
         December 2011 - July 2013
@@ -81,7 +89,7 @@
 
     <li>
       <address>
-        <a href="https://goo.gl/maps/f7uwf">
+        <a href="https://www.google.com/maps/place/3160+Rubino+Dr,+San+Jose,+CA+95125/@37.2796545,-121.875305,1145m/data=!3m2!1e3!4b1!4m5!3m4!1s0x808e33a478df8cf9:0xe50b8d240bdba501!8m2!3d37.2796545!4d-121.8731163?hl=en">
         3160 Rubino Drive #103<br>
         San Jose, California 95125-6383<br></a>
         February 2011 - December 2011
@@ -90,7 +98,7 @@
 
     <li>
       <address>
-        <a href="https://goo.gl/maps/rK5Ru">
+        <a href="https://www.google.com/maps/place/1749+Seagull+Ct,+Reston,+VA+20194/@38.967501,-77.36002,559m/data=!3m1!1e3!4m5!3m4!1s0x89b637e14c22afed:0x147b49dea3e48f82!8m2!3d38.9673372!4d-77.3602266?hl=en">
         1742 Seagull Court #101<br>
         Reston, Virgina 20194-4336<br></a>
         November 2010 - February 2011
@@ -99,7 +107,7 @@
 
     <li>
       <address>
-        <a href="https://goo.gl/5o6Z1X">
+        <a href="https://www.google.com/maps/place/19465+42nd+Ave+SE+%2328e,+Bothell,+WA+98012/@47.8351068,-122.1777418,676m/data=!3m1!1e3!4m5!3m4!1s0x549008b2083577eb:0x1a2d17570477366!8m2!3d47.8351068!4d-122.1755531">
         19465 42nd Avenue Southeast #28E<br>
         Bothell, Washington 98012-5446<br></a>
         January 2010 - October 2010
@@ -108,7 +116,7 @@
 
     <li>
       <address>
-        <a href="https://goo.gl/piH53o">
+        <a href="https://www.google.com/maps/place/1250+W+Grove+Pkwy+%231178,+Tempe,+AZ+85283/@33.3530065,-111.9606296,1202m/data=!3m2!1e3!4b1!4m5!3m4!1s0x872b05dc7857c2e9:0x8b9a2d55ae0bf8fa!8m2!3d33.352613!4d-111.961096">
         1250 West Grove Parkway #1178<br>
         Tempe, Arizona 85283-4449<br></a>
         June 2007 - January 2010
@@ -117,7 +125,7 @@
 
     <li>
       <address>
-        <a href="https://goo.gl/Raicxa">
+        <a href="https://www.google.com/maps/place/12500+Merit+Dr+%236312,+Dallas,+TX+75251/@32.9179083,-96.7741388,852m/data=!3m1!1e3!4m5!3m4!1s0x864c213d33e80243:0xa1328427fb6b05f7!8m2!3d32.9179083!4d-96.7719501?hl=en">
         12500 Merit Dr #6312<br>
         Dallas, Texas 75251-1933<br></a>
         October 2006 - June 2007
@@ -126,7 +134,7 @@
 
     <li>
       <address>
-        <a href="https://goo.gl/maps/Eqzg1">
+        <a href="https://www.google.com/maps/place/6265+Kelez+Ct,+San+Jose,+CA+95120/@37.228945,-121.871311,573m/data=!3m1!1e3!4m5!3m4!1s0x808e311336728481:0x2db0b24d002b1ce0!8m2!3d37.2287906!4d-121.8718177?hl=en">
         6265 Kelez Ct<br>
         San Jose, California 95120-2819<br></a>
         December 2004 - October 2006
@@ -135,7 +143,7 @@
 
     <li>
       <address>
-        <a href="https://goo.gl/Bor4KG">
+        <a href="https://www.google.com/maps/place/2800+Madison+Ave,+Fullerton,+CA+92831/@33.8825918,-117.8812601,841m/data=!3m1!1e3!4m5!3m4!1s0x80dcd5cd3ee3e197:0xeefcbbdb58ac36ae!8m2!3d33.8825918!4d-117.8790714">
           2800 Madison Ave #C5<br>
           Fullerton, California 92831-2231</a><br>
           May 2004 - December 2004
@@ -144,7 +152,7 @@
 
     <li>
       <address>
-        <a href="https://goo.gl/maps/w1uZC">
+        <a href="https://www.google.com/maps/place/Camino+Verde+Dr,+San+Jose,+CA+95119/@37.234655,-121.800833,573m/data=!3m1!1e3!4m5!3m4!1s0x808e2e2d449e27af:0x65925660dccb483c!8m2!3d37.2325403!4d-121.7992525?hl=en">
         6168 Camino Verde Way #???<br>
         San Jose, California 95119-1407<br></a>
         July 2003 - May 2004
@@ -153,7 +161,7 @@
 
     <li>
       <address>
-        <a href="https://goo.gl/maps/nnyJq">
+        <a href="https://www.google.com/maps/place/6187+Ellerbrook+Way,+San+Jose,+CA+95123/@37.233019,-121.809851,573m/data=!3m1!1e3!4m5!3m4!1s0x808e2e2a1cefe231:0x4377956d94fce84d!8m2!3d37.2330163!4d-121.8098625?hl=en">
         6187 Ellerbrook Way<br>
         San Jose, California 95123-5012<br></a>
         November 2001 - July 2003
@@ -162,7 +170,7 @@
 
     <li>
       <address>
-        <a href="https://goo.gl/TNsEPH">
+        <a href="https://www.google.com/maps/place/5485+Eagles+Ln+APT+3,+San+Jose,+CA+95123/@37.2541171,-121.8221545,1145m/data=!3m2!1e3!4b1!4m5!3m4!1s0x808e31f0079081c7:0x2364ac58f49d0860!8m2!3d37.2541014!4d-121.8221742?hl=en">
         5485 Eagles Lane #3<br>
         San Jose, California 95123-1869<br></a>
         November 2000 - November 2001
@@ -171,7 +179,7 @@
 
     <li>
       <address>
-        <a href="https://goo.gl/TNsEPH">
+        <a href="https://www.google.com/maps/place/5485+Eagles+Ln+APT+3,+San+Jose,+CA+95123/@37.2541171,-121.8221545,1145m/data=!3m2!1e3!4b1!4m5!3m4!1s0x808e31f0079081c7:0x2364ac58f49d0860!8m2!3d37.2541014!4d-121.8221742?hl=en">
         5475 Eagles Lane #4<br>
         San Jose, California 95123-1868<br></a>
       </address>
@@ -179,7 +187,7 @@
 
     <li>
       <address>
-        <a href="https://goo.gl/TLyAHc">
+        <a href="https://www.google.com/maps/place/1901+Halford+Ave,+Santa+Clara,+CA+95051/@37.3574213,-121.9971438,1144m/data=!3m2!1e3!4b1!4m5!3m4!1s0x808fb5f99f913413:0x1c8d792514a166f!8m2!3d37.3570176!4d-121.9974058?hl=en">
         1901 Halford Ave<br>
         Santa Clara, California 95051<br></a>
       </address>
@@ -187,7 +195,7 @@
 
     <li>
       <address>
-        <a href="https://goo.gl/maps/4I4G0">
+        <a href="https://www.google.com/maps/place/1408+Essex+Way,+San+Jose,+CA+95117/@37.298841,-121.956568,286m/data=!3m1!1e3!4m5!3m4!1s0x808e352d226cab7b:0xb670722b97708ac!8m2!3d37.298934!4d-121.9562653?hl=en">
         1408 Essex Way #1<br>
         San Jose, California 95117<br></a>
       </address>
@@ -195,7 +203,7 @@
 
     <li>
       <address>
-        <a href="https://goo.gl/maps/0Ufi2">
+        <a href="https://www.google.com/maps?q=Pruneridge+Avenue,+Santa+Clara,+CA&hl=en&ll=37.331341,-121.996999&spn=0.004554,0.003637&sll=37.298841,-121.956568&sspn=0.002182,0.001824&oq=prune&t=h&hnear=Pruneridge+Ave,+Santa+Clara,+California&z=18">
         3655 Pruneridge Ave #???<br>
         Santa Clara, California 95051<br></a>
       </address>
@@ -203,7 +211,7 @@
 
     <li>
       <address>
-        <a href="https://goo.gl/maps/0Ufi2">
+        <a href="https://www.google.com/maps/place/Garland,+TX/@32.9078253,-96.6759224,38653m/data=!3m2!1e3!4b1!4m5!3m4!1s0x864c03c1db5fc275:0x71fbdcb38623cd0!8m2!3d32.912624!4d-96.6388833?hl=en">
         ???<br>
         Garland, Texas 75040<br></a>
       </address>
@@ -211,7 +219,7 @@
 
     <li>
       <address>
-        <a href="#">
+        <a href="https://www.google.com/maps/place/549+E+Blancke+St,+Linden,+NJ+07036/@40.6369807,-74.2517977,546m/data=!3m1!1e3!4m5!3m4!1s0x89c3b2e31d444e2f:0x215105d2de63537c!8m2!3d40.637023!4d-74.2510146">
         549 East Blancke Street<br>
         Linden, New Jersey 07036<br></a>
       </address>
@@ -219,14 +227,14 @@
 
     <li>
       <address>
-        <a href="#">
+        <a href="https://www.google.com/maps/place/Parsippany-Troy+Hills,+NJ/@40.8634297,-74.5074278,34819m/data=!3m2!1e3!4b1!4m5!3m4!1s0x89c30824287028c3:0xf6a59890b16324f0!8m2!3d40.8652865!4d-74.4173877">
         Route 4, Parsippany, NJ 07054</a
       </address>
     </li>
 
     <li>
       <address>
-        <a href="#">
+        <a href="https://www.google.com/maps/place/549+E+Blancke+St,+Linden,    +NJ+07036/@40.6369807,-74.2517977,546m/data=!3m1!1e3!4m5!3m4!1s0x89c3b2e31d4    44e2f:0x215105d2de63537c!8m2!3d40.637023!4d-74.2510146">
         549 East Blancke Street<br>
         Linden, New Jersey 07036<br></a>
       </address>
@@ -234,7 +242,7 @@
 
     <li>
       <address>
-        <a href="#">
+        <a href="https://www.google.com/maps/place/116+E+Henry+St,+Linden,+NJ+07036/@40.6348901,-74.2576189,459m/data=!3m1!1e3!4m5!3m4!1s0x89c3b2fd8814d09b:0x1e31ee27d7304e56!8m2!3d40.635229!4d-74.2566614">
         116 East Henry Street<br>
         Linden, New Jersey 07036<br></a>
       </address>
