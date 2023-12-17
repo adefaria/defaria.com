924a8eeb5be3bd46a3031d43d0f85318b199b5f0
Andrew DeFaria <Andrew@DeFaria.com>
2023-12-17 10:04:13 -0800

    Added code for displaying images
    Currently turned off because 1) images are irregular and I
    should be getting more pics tonight.
    
    Also added hearts for the list and favicon.
    
    Changed to start random song.

--- ali/Ali.js
+++ ali/Ali.js
@@ -1,26 +1,59 @@
 const player = document.getElementById("player");
 const links = document.querySelectorAll("a[data-song-url]");
-//const title = document.getElementById("songTitle");
 const story = document.getElementById("story");
 const songsList = document.getElementById("songs");
 const songs = Array.from(songsList.querySelectorAll("a"));
-// const songSelection = document.getElementById("songSelection");
+const images = [
+  { url: "Images/Ali1.jpg" },
+  { url: "Images/Ali2.jpg" },
+  { url: "Images/Ali3.jpg" },
+  { url: "Images/Ali4.jpg" },
+  { url: "Images/Ali5.jpg" },
+  { url: "Images/Ali6.jpg" },
+  { url: "Images/Ali7.jpg" },
+];
 
 nextSong = 0;
 
 player.addEventListener("ended", playNext);
 
+function setImages() {
+  const randomIndexLeft = Math.floor(Math.random() * images.length);
+
+  randomIndexRight = randomIndexLeft;
+
+  // Don't want two of the same pics so loop until we find a different one
+  while (randomIndexLeft == randomIndexRight) {
+    randomIndexRight = Math.floor(Math.random() * images.length);
+  }
+
+  const selectedImageLeft = images[randomIndexLeft];
+  const selectedImageRight = images[randomIndexRight];
+
+  const leftPic = document.getElementById("leftImage");
+  const rightPic = document.getElementById("rightImage");
+
+  if (leftPic != null) {
+    leftPic.src = selectedImageLeft.url;
+  }
+  if (rightPic != null) {
+    rightPic.src = selectedImageRight.url;
+  }
+}
+
 function displayStoryAndPlay(songUrl) {
   // Format songName
   extensionIndex = songUrl.lastIndexOf(".");
   songName = songUrl.substring(6, extensionIndex);
-  //title.innerHTML = songName;
 
   // Set story for song - look for songName.html
   const songsStory = "Stories/" + songName + ".html";
 
   // Change the iFrame
   story.src = songsStory;
+
+  // Change images
+  setImages();
 
   player.src = songUrl;
   player.play();
